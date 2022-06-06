c21f1df197e272d4dcedd3367351fba298f8a30f
yoho <panyoho@gmail.com>
2022-06-06 19:06:07 +0800

    chore: update test

--- playground/worker/classic-worker.js
+++ playground/worker/classic-worker.js
@@ -1,3 +1,10 @@
+import mods from './modules/module2.js'
+
+self.postMessage({
+  type: 'classic-worker-import',
+  content: JSON.stringify(mods)
+})
+
 let base = `/${self.location.pathname.split('/')[1]}`
 if (base === `/worker-entries`) base = '' // relative base
 
@@ -7,13 +14,6 @@ self.addEventListener('message', () => {
   self.postMessage({
     type: 'classic-worker-import-script',
     content: self.constant
-  })
-})
-
-import('./modules/module2.js').then(mods => {
-  self.postMessage({
-    type: 'classic-worker-import',
-    content: JSON.stringify(mods)
   })
 })
 
